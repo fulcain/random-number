2dc8ae497353699d28cc4c0935c736b507094096
Majid Kargar <majidkargar01@gmail.com>
2024-11-07 11:46:44 +0330

    Refactor: formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-		<>
-			<h2>App</h2>
+    <>
+      <h2>App</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
